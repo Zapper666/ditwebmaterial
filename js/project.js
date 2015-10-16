2559934c73a84438ad573cf19c0bb9407923e8b5
iZapper <yahyaragil@gmail.com>
2015-10-16 14:53:11 +0700

    Update Web Content

--- js/project.js
+++ js/project.js
@@ -580,6 +580,20 @@ function createPile(transform, num){
 })();
 
 
+//Chart Dapodik//
+
+$(function(){
+      var $ppc = $('.progress-pie-chart'),
+        percent = parseInt($ppc.data('percent')),
+        deg = 360*percent/100;
+      if (percent > 50) {
+        $ppc.addClass('gt-50');
+      }
+      $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
+      $('.ppc-percents span').html(percent+'%');
+    });
+   
+
 //================//
   //Mobile View//
 //===============//
